581c1388916e05266cf78b20ed4d77c9c9f44653
Klint Sulstarova <klint.sulstarova@stud.uni-bamberg.de>
2020-04-16 22:14:37 +0200

    vk

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const router = express.Router();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const keys = require("../config/keys");
